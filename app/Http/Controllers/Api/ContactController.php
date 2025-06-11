f99117887683a90ffa59034e23293afa672fe151
Yazil Ansari <yazilansari@gmail.com>
2025-06-11 12:11:57 +0400

    Added Campaign Route

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -73,4 +73,59 @@ class ContactController extends Controller
             ]);
         }
     }
+
+    public function campaign(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required|string',
+            'email'     => 'required|string',
+            'subject'     => 'required|string',
+            'message'  => 'required|string'
+            ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $mail = new PHPMailer(true);
+    
+        try {
+    
+            /* Email SMTP Settings */
+            $mail->SMTPDebug = 0;
+            $mail->isSMTP();
+            $mail->Host = env('MAIL_HOST');
+            $mail->SMTPAuth = true;
+            $mail->Username = env('MAIL_USERNAME');
+            $mail->Password = env('MAIL_PASSWORD');
+            $mail->SMTPSecure = env('MAIL_ENCRYPTION');
+            $mail->Port = env('MAIL_PORT');
+    
+            $mail->setFrom(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'));
+            $mail->addAddress('estore.om@ahmedalmaghribi.com');
+    
+            $mail->isHTML(true);
+    
+            $mail->Subject = $request->subject;
+            $mail->Body    = $request->name.'<br><br>'.$request->email.'<br><br>'.$request->message.'<br><br><b>***This form is submitted for Fathers Day***</b>';
+            
+    
+            if( !$mail->send() ) {
+                return response()->json([
+                    'message' => 'Oops... There is an Error'
+                ]);
+            }
+                
+            else {
+                 return response()->json([
+                    'message' => 'Contact Enquiry Submitted Successfully'
+                ]);
+            }
+    
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Oops... There is an Exception'
+            ]);
+        }
+    }
 }
