63e648b75efe18bb4370a1844ef21c8466b7455e
Yazil Ansari <yazilansari@gmail.com>
2025-06-12 12:21:26 +0400

    Changed Mail Condition for Collections

--- platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
+++ platform/plugins/ecommerce/src/Services/CreatePaymentForOrderService.php
@@ -209,7 +209,7 @@ class CreatePaymentForOrderService
                                                                                                                                 </td>
                                                                                                                             </tr>';
                                                                                                                         }
-                                                                                                                    } else if($value->sale_price != 0) {
+                                                                                                                    } else if($value->product_category == 'Collections') {
                                                                                                                         $body .= '<tr>
                                                                                                                             <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->product_name.'</div>
@@ -218,7 +218,7 @@ class CreatePaymentForOrderService
                                                                                                                                 <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">'.$value->qty.'</div>
                                                                                                                             </td>
                                                                                                                             <td style="color:#636363;text-align:left;vertical-align:middle;padding:12px;border:1px solid #E5E5E5;">
-                                                                                                                                <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x0631;&#x002E;&#x0639;'.number_format(((float) ($value->price * 1.05) - ((($value->price * 1.05) / 100) * $value->sale_price) * $value->qty), 3, '.', '').'</div>
+                                                                                                                                <div style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;text-align:left;">&#x0631;&#x002E;&#x0639;'.number_format(((float) ($value->gross_amount)), 3, '.', '').'</div>
                                                                                                                             </td>
                                                                                                                         </tr>';
                                                                                                                     } else {
